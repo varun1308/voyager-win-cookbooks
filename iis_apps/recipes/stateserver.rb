40ccf02ab33a9829fe6f84de2ad9dbc176330ae4
VARUN JAIN <varun13@gmail.com>
2015-11-06 12:00:34 +0530

    added state server recipe

--- iis_apps/recipes/stateserver.rb
+++ iis_apps/recipes/stateserver.rb
@@ -13,7 +13,7 @@ powershell_script 'install ASP.NET State Service if not already installed' do
 				Add-WindowsFeature Web-Asp-Net
 		     }
 		  EOH
-		  notifies :run, "windows_service['ASP.NET State Service']", :immediately
+		  notifies :run, "windows_service[ASP.NET State Service]", :immediately
 		end
 
 #configure asp.net state service
