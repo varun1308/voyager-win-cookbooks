7f7062f6d2211b2eb6a871ec8bbd5348390060c2
VARUN JAIN <varun13@gmail.com>
2015-11-19 22:45:35 +0530

    added CNAME and A records for route53

--- other_apps/recipes/routing_services.rb
+++ other_apps/recipes/routing_services.rb
@@ -44,28 +44,46 @@ if node["env"]
 	env = node["env"] + '-'
 end
 
-Chef::Log.debug "Found records: #{records}"
 Chef::Log.debug "Found CNAME: #{records['CNAME']}"
 
+#for CNAME and A records, add routes
 
-records.each do |type, items|
-	items.each do |key, values|
-		values.each do |value|
-			#prepend env indicator to records
-			#update route53 entries for all record pairs
-			route53_record "create a record for #{value}" do
-			  name  	"#{env}#{value}"
-			  value 	key
-			  type  	type
-			  ttl		60
-			  zone_id 	zone_id["id"]
-			  #aws_access_key_id     node[:custom_access_key]
-			  #aws_secret_access_key node[:custom_secret_key]
-			  overwrite true
-			  action :create
-			end
+records['CNAME'].each do |key, values|
+	values.each do |value|
+		Chef::Log.debug "Found record: #{key}, #{value}"
+		#prepend env indicator to records
+		#update route53 entries for all record pairs
+		route53_record "create a record for #{value}" do
+		  name  	"#{env}#{value}"
+		  value 	key
+		  type  	'CNAME'
+		  ttl		60
+		  zone_id 	zone_id["id"]
+		  #aws_access_key_id     node[:custom_access_key]
+		  #aws_secret_access_key node[:custom_secret_key]
+		  overwrite true
+		  action :create
 		end
 	end
 end
 
+records['A'].each do |key, values|
+	values.each do |value|
+		Chef::Log.debug "Found record: #{key}, #{value}"
+		
+		#prepend env indicator to records
+		#update route53 entries for all record pairs
+		route53_record "create a record for #{value}" do
+		  name  	"#{env}#{value}"
+		  value 	key
+		  type  	'A'
+		  ttl		60
+		  zone_id 	zone_id["id"]
+		  #aws_access_key_id     node[:custom_access_key]
+		  #aws_secret_access_key node[:custom_secret_key]
+		  overwrite true
+		  action :create
+		end
+	end
+end
 
