d50b97fc9c32dab5a6d78cc3214d8c9b6baef4fc
VARUN JAIN <varun13@gmail.com>
2015-11-24 19:36:56 +0530

    removed !

--- iis_apps/providers/website.rb
+++ iis_apps/providers/website.rb
@@ -26,7 +26,7 @@ action :add do
 		file_name, bucket, remote_path, url = Tavisca::WinApps::Helper.parse_uri(new_resource.scm[:url])
 
 		#replace build number
-		file_name.sub('build_number', new_resource.build_number)!
+		file_name = file_name.sub('build_number', new_resource.build_number)
 
 		#download file from s3
 		aws_s3_file ::File.join(Chef::Config["file_cache_path"],file_name) do
